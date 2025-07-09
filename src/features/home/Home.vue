545f4de430c72c7afd02c78f3494f59944e66148
YasakaKanoko <87556577+YasakaKanoko@users.noreply.github.com>
2025-07-10 00:32:36 +0800

    refactor(src/features/home/Home.vue): Update grid item navigation
    - Updated the navigation within the grid items to use `router.push` instead of calling `changeComponent`.
    - Added a class `item` to each `van-grid-item` for potential styling improvements.

--- src/features/home/Home.vue
+++ src/features/home/Home.vue
@@ -1,26 +1,26 @@
 <template>
   <van-grid :border="false" :column-num="3" gutter="10">
-    <van-grid-item @click="changeComponent('Temperature')">
+    <van-grid-item class="item" @click="router.push('/temperature')">
       <van-image src="temperature.svg" />
     </van-grid-item>
-    <van-grid-item>
+    <van-grid-item class="item">
       <van-image src="length.svg" />
     </van-grid-item>
-    <van-grid-item>
+    <van-grid-item class="item">
       <van-image src="weight.svg" />
     </van-grid-item>
-    <van-grid-item>
+    <van-grid-item class="item">
       <van-image src="currency.svg" />
     </van-grid-item>
-    <van-grid-item>
+    <van-grid-item class="item">
       <van-image src="memo.svg" />
     </van-grid-item>
   </van-grid>
 </template>
 
 <script setup>
-defineProps(['changeComponent']);
-
+import { useRouter } from 'vue-router'
+const router = useRouter()
 </script>
 
 <style scoped>
